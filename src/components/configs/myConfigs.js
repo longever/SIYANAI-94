ac3b05109087f31a001f6a1fd12b6d7cf9296e39
Peter Pan <longever@gmail.com>
2025-10-31 10:35:21 +0800

    feat: AI 生成文件 version 325

---
+++ src/components/configs/myConfigs.js
@@ -0,0 +1,6 @@
+
+export const FUNCTION_TEXT_TO_VIDEO = 'text-to-video';
+export const FUNCTION_IMAGE_DESCRIPTION_TO_VIDEO = 'image-description-to-video';
+export const FUNCTION_IMAGE_AUDIO_TO_VIDEO = 'image-audio-to-video';
+export const FUNCTION_IMAGE_VIDEO_TO_VIDEO = 'image-video-to-video';
+  
