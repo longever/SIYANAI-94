abc8212840d1902c8f7a4500b449d82d105fac8d
Peter Pan <longever@gmail.com>
2025-10-31 17:53:57 +0800

    快捷编辑文件: components/configs/myConfigs.js

--- src/components/configs/myConfigs.js
+++ src/components/configs/myConfigs.js
@@ -1,5 +1,5 @@
 
-export const FUNCTION_TEXT_TO_VIDEO = 'description-to-video'; //'ai-video-creator';//
-export const FUNCTION_IMAGE_DESCRIPTION_TO_VIDEO = 'image-prompt-to-video-task';// 'ai-video-creator';
+export const FUNCTION_TEXT_TO_VIDEO ='ai-video-creator';// 'description-to-video'; //
+export const FUNCTION_IMAGE_DESCRIPTION_TO_VIDEO = 'ai-video-creator'; // 'image-prompt-to-video-task';//
 export const FUNCTION_IMAGE_AUDIO_TO_VIDEO = 'ai-video-creator';//'image-audio-to-video';
 export const FUNCTION_IMAGE_VIDEO_TO_VIDEO = 'ai-video-creator';//'image-video-to-video';
